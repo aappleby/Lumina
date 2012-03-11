a8d589f028a6b451673218f892b28728fc5b504d
Austin Appleby <aappleby@gmail.com>
2012-03-11 16:21:04 -0700

    checkpoint

--- src/LuaString.h
+++ src/LuaString.h
@@ -7,11 +7,9 @@
 __declspec(align(8)) class TString : public LuaBase {
 public:
 
-  const char * c_str() const { return reinterpret_cast<const char *>(this+1); }
-
-protected:
   uint8_t reserved;
   unsigned int hash;
   size_t len;  /* number of characters in string */
 
+  const char * c_str() const { return reinterpret_cast<const char *>(this+1); }
 };
