b2f6836b1b465275481b042f96d83ac334747659
Austin Appleby <aappleby@gmail.com>
2012-03-11 14:50:31 -0700

    checkpoint

---
+++ src/LuaTypes.h
@@ -0,0 +1,52 @@
+#pragma once
+#include "stdint.h"
+
+class LuaBase;
+struct lua_State;
+
+
+#define LUA_NUMBER_DOUBLE
+#define LUA_NUMBER	double
+
+/*
+@@ LUA_INTEGER is the integral type used by lua_pushinteger/lua_tointeger.
+** CHANGE that if ptrdiff_t is not adequate on your machine. (On most
+** machines, ptrdiff_t gives a good choice between int or long.)
+*/
+#define LUA_INTEGER	ptrdiff_t
+
+/*
+@@ LUA_UNSIGNED is the integral type used by lua_pushunsigned/lua_tounsigned.
+** It must have at least 32 bits.
+*/
+#define LUA_UNSIGNED	uint32_t
+
+
+typedef int (*lua_CFunction) (lua_State *L);
+
+
+
+/* type of numbers in Lua */
+typedef LUA_NUMBER lua_Number;
+
+
+/* type for integer functions */
+typedef LUA_INTEGER lua_Integer;
+
+/* unsigned integer type */
+typedef LUA_UNSIGNED lua_Unsigned;
+
+
+
+
+/*
+@@ LUA_INT32 is an signed integer with exactly 32 bits.
+@@ LUAI_UMEM is an unsigned integer big enough to count the total
+@* memory used by Lua.
+@@ LUAI_MEM is a signed integer big enough to count the total memory
+@* used by Lua.
+** CHANGE here if for some weird reason the default definitions are not
+** good enough for your machine. Probably you do not need to change
+** this.
+*/
+#define LUAI_MEM	ptrdiff_t
---
+++ src/LuaValue.cpp
@@ -0,0 +1,1 @@
+#include "LuaValue.h"
